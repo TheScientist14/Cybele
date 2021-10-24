32b06ba91968f1646ff0868580de50edf2375296
TheScientist14 <saurytom@gmail.com>
2021-10-24 12:02:49 +0200

    Dialog and tuto wip

---
+++ Assets/TextManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public class TextManager : MonoBehaviour
+{
+    public GameObject phrygien;
+    public GameObject romain;
+    public TextMeshProUGUI textPanel;
+
+    public UnityEvent NextTextEvent;
+
+    public static TextManager instance;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        NextTextEvent = new UnityEvent();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.instance.SetIsRunning(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetText(Text text)
+    {
+        textPanel.text = text.text;
+    }
+
+    public void NextText()
+    {
+        NextTextEvent.Invoke();
+    }
+}
